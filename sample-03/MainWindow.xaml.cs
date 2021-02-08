c99fd2a3b2f5415a9605481c38ebc338c393802f
paul.kazusek <paul.kazusek@gmail.com>
2021-02-08 23:52:34 +0100

    add sampe to draw a rect

---
+++ sample-03/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sample_03
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            // the canvas and properties
+            var canvas = e.Surface.Canvas;
+            var width = e.Info.Width;
+            var height = e.Info.Height;
+
+            // get the screen density for scaling
+            var scale = (float)PresentationSource.FromVisual(this).CompositionTarget.TransformToDevice.M11;
+            var scaledSize = new SKSize(e.Info.Width / scale, e.Info.Height / scale);
+
+            // handle the device screen density
+            canvas.Scale(scale);
+
+            // make sure the canvas is blank
+            canvas.Clear(SKColors.White);
+
+            SKPaint paintRect = new SKPaint()
+            {
+                Style = SKPaintStyle.StrokeAndFill,
+                IsAntialias = true,
+                Color = SKColors.SteelBlue,
+                StrokeWidth = 1,
+            };
+
+            float left = (width / 2.0f) - 100;
+            float right = (width / 2.0f) + 100;
+            float top = (height / 2.0f) - 100;
+            float bottom = (height / 2.0f) + 100;
+
+            SKRect rect = new SKRect(left, top, right, bottom);
+
+            canvas.DrawRect(rect, paintRect);
+        }
+    }
+}
