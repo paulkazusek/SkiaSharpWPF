9aecaf28a4a78ffc1fb9bedfe6f8d566c9610764
paul.kazusek <paul.kazusek@gmail.com>
2021-02-08 23:33:50 +0100

    add sampe to draw some text

--- sample-01/MainWindow.xaml.cs
+++ sample-01/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SkiaSharp;
+using SkiaSharp.Views.Desktop;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace sample_01
 {
@@ -24,5 +13,42 @@ namespace sample_01
         {
             InitializeComponent();
         }
-    }
+
+		private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+		{
+			// the canvas and properties
+			var canvas = e.Surface.Canvas;
+			var width = e.Info.Width;
+			var height = e.Info.Height;
+
+			// get the screen density for scaling
+			var scale = (float)PresentationSource.FromVisual(this).CompositionTarget.TransformToDevice.M11;
+			var scaledSize = new SKSize(e.Info.Width / scale, e.Info.Height / scale);
+
+			// handle the device screen density
+			canvas.Scale(scale);
+
+			// make sure the canvas is blank
+			canvas.Clear(SKColors.White);
+
+            // draw some text
+            SKTypeface font = SKTypeface.FromFamilyName("Arial", SKFontStyle.Normal);
+            SKColor steelBlue = SKColors.SteelBlue;
+
+            SKPaint paintText = new SKPaint()
+            {
+                Style = SKPaintStyle.Fill,
+                IsAntialias = true,
+                Color = steelBlue,
+                TextSize = 32,
+                TextAlign = SKTextAlign.Center,
+                Typeface = font,
+            };
+
+            SKSize canvasSize = new SKSize(width, height);
+            SKPoint center = new SKPoint(canvasSize.Width / 2f, canvasSize.Height / 2);
+
+            canvas.DrawText("Hello World", center, paintText);
+        }
+	}
 }
